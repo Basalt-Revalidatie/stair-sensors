05ea2866bc2a0d757aaf1e35957ed6abf1d27071
Klaas Schoute <klaas_schoute@hotmail.com>
2023-09-10 17:43:46 +0200

    Add reconnect function

--- src/communications/wifi.h
+++ src/communications/wifi.h
@@ -17,11 +17,28 @@ extern int16_t sensorID;
 WiFiClient espClient;
 
 /**
+ * @brief Callback function for when the WiFi connection is lost
+ * 
+ * @param event
+ * @param info
+ */
+void Wifi_disconnected(WiFiEvent_t event, WiFiEventInfo_t info){
+    Serial.println("Disconnected from WIFI access point");
+    Serial.print("WiFi lost connection. Reason: ");
+    Serial.println(info.wifi_sta_disconnected.reason);
+    Serial.println("Reconnecting...");
+    WiFi.begin(ssid, password);
+}
+
+/**
  * @brief Setup the WiFi connection
  */
 void setupWiFi() {
     WiFi.mode(WIFI_STA); //Optional
     WiFi.setHostname(("Stair Sensor-" + String(sensorID)).c_str());
+
+    WiFi.onEvent(Wifi_disconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+
     WiFi.begin(ssid, password);
     Serial.println("\nConnecting");
 
