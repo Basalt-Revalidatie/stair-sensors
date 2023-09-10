80548f4a9a966d8a6c988e79c9b2ac2729361187
Klaas Schoute <klaas_schoute@hotmail.com>
2023-09-10 17:44:35 +0200

    Add reconnect function and set wifi hostname (#6)
    * Set the wifi hostname with sensorID
    
    * Add reconnect function

--- src/communications/wifi.h
+++ src/communications/wifi.h
@@ -12,13 +12,33 @@
 // Project includes
 #include "secrets/config.h"
 
+extern int16_t sensorID;
+
 WiFiClient espClient;
+
+/**
+ * @brief Callback function for when the WiFi connection is lost
+ * 
+ * @param event
+ * @param info
+ */
+void Wifi_disconnected(WiFiEvent_t event, WiFiEventInfo_t info){
+    Serial.println("Disconnected from WIFI access point");
+    Serial.print("WiFi lost connection. Reason: ");
+    Serial.println(info.wifi_sta_disconnected.reason);
+    Serial.println("Reconnecting...");
+    WiFi.begin(ssid, password);
+}
 
 /**
  * @brief Setup the WiFi connection
  */
 void setupWiFi() {
     WiFi.mode(WIFI_STA); //Optional
+    WiFi.setHostname(("Stair Sensor-" + String(sensorID)).c_str());
+
+    WiFi.onEvent(Wifi_disconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+
     WiFi.begin(ssid, password);
     Serial.println("\nConnecting");
 
@@ -27,7 +47,10 @@ void setupWiFi() {
         delay(100);
     }
 
+    // Show connection details
     Serial.println("\nConnected to the WiFi network");
     Serial.print("Local ESP32 IP: ");
     Serial.println(WiFi.localIP());
+    Serial.print("Default Hostname: ");
+    Serial.println(WiFi.getHostname());
 }
