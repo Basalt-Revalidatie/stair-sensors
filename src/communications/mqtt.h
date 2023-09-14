579b0efd279841239df855bfdd2060e667724bff
Klaas Schoute <klaas_schoute@hotmail.com>
2023-09-14 23:13:46 +0200

    Send json mqtt message on trigger (#8)

--- src/communications/mqtt.h
+++ src/communications/mqtt.h
@@ -9,12 +9,26 @@
 
 #include <PubSubClient.h>
 #include <WiFiClient.h>
+#include <ArduinoJson.h>
 
 extern const char *subscribeTopic;
 extern int16_t sensorID;
 
 extern WiFiClient espClient;
 PubSubClient client(espClient);
+
+// MQTT topics
+const char *subscribeTopic = "settings";
+
+/**
+ * @brief Generate a topic for the sensor
+ * 
+ * @param type 
+ * @return String 
+ */
+String generateTopic(String type) {
+  return "sensor/" + String(sensorID) + "/" + String(type) ;
+}
 
 void callback(char *topic, byte *payload, unsigned int length) {
   Serial.print("Message arrived [");
@@ -45,6 +59,20 @@ void reconnect() {
       delay(5000);
     }
   }
+}
+
+void trigger(int distance, int max_distance, int threshold) {
+  // Create a JSON object and populate it
+  StaticJsonDocument<200> jsonDoc;
+
+  jsonDoc["distance"] = distance;
+  jsonDoc["max_distance"] = max_distance;
+  jsonDoc["threshold"] = threshold;
+
+  // Serialize JSON object to a string buffer
+  char jsonBuffer[200];
+  serializeJson(jsonDoc, jsonBuffer);
+  client.publish(generateTopic("trigger").c_str(), jsonBuffer);
 }
 
 void setupMQTT() {
